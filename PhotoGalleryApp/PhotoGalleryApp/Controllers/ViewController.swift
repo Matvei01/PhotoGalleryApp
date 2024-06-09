f6c8cc1114fea972baad121c19af6327995610e5
Matvei <khlestov.matvei@mail.ru>
2024-06-09 10:50:06 +0500

    Created base project, removed storyboard references

---
+++ PhotoGalleryApp/PhotoGalleryApp/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by Matvei Khlestov on 09.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
---
+++ PhotoGalleryApp/PhotoGalleryApp/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  PhotoGalleryApp
+//
+//  Created by Matvei Khlestov on 09.06.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
